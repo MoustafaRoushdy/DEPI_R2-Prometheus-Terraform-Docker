088c79d8d11460db8d126cb5403ca70aa3630d74
MohammedAhmedTaha <mohamed.a.taha2222@gmail.com>
2025-04-12 16:07:39 +0200

    Added prometheus setup with docker and updated key pair

--- main.tf
+++ main.tf
@@ -34,12 +34,31 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "prometheus_server" {
@@ -63,7 +82,4 @@ resource "aws_instance" "prometheus_server" {
   }
 }
 
-output "prometheus_public_ip" {
-  value       = aws_instance.prometheus_server.public_ip
-  description = "Public IP of the Prometheus EC2 instance"
-}
+
