708d0d2c4f202774332a4e242e850101a46d40ab
Ahmed Abualkhair <ahmedabualkhair01@gmail.com>
2025-04-20 00:24:56 +0200

    Last

--- main.tf
+++ main.tf
@@ -8,11 +8,25 @@ module "jenkins_server" {
 }
 
 
-module "prometheus_server" {
+module "prometheus_server_2" {
   source = "./modules/server_module"
-  server_name = "prometheus"
+  server_name = "prometheus_server_2"
   script_path = "install_docker_prometheus.sh"
   web_port = 9090
   is_file_copied = true
   file_name = "prometheus.yml"
+}
+
+module "jenkins_node_1" {
+  source = "./modules/server_module"
+  server_name = "jenkins_node_1"
+  script_path = "install_openjdk.sh"
+  web_port = 8081
+}
+
+module "jenkins_node_2" {
+  source = "./modules/server_module"
+  server_name = "jenkins_node_2"
+  script_path = "install_openjdk.sh"
+  web_port = 8082
 }
--- scripts/install_openjdk.sh
+++ scripts/install_openjdk.sh
@@ -1,6 +1,5 @@
-set -e
 sudo apt update -y
 sudo apt install openjdk-17-jdk -y
-java -version
+
 
 
