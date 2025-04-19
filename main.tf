fc1676b81850493830fa94b35b7e2486fc0e9c85
Shehab Ahmed <shehabeldeenahmed727@gmail.com>
2025-04-19 18:18:09 +0200

    jenkins nodes added

--- main.tf
+++ main.tf
@@ -1,18 +1,31 @@
-
-
-module "jenkins_server" {
+module "jenkins_node_1" {
   source = "./modules/server_module"
-  server_name = "jenkins"
+  server_name = "node_1"
   script_path = "install_java.sh"
-  web_port = 8080
+  web_port = 8080 
 }
 
-
-module "prometheus_server" {
+module "jenkins_node_2" {
   source = "./modules/server_module"
-  server_name = "prometheus"
-  script_path = "install_docker_prometheus.sh"
-  web_port = 9090
-  is_file_copied = true
-  file_name = "prometheus.yml"
+  server_name = "node_2"
+  script_path = "install_java.sh"
+  web_port = 8080 
 }
+
+
+# module "jenkins_server" {
+#   source = "./modules/server_module"
+#   server_name = "jenkins"
+#   script_path = "install_java.sh"
+#   web_port = 8080
+# }
+
+
+# module "prometheus_server" {
+#   source = "./modules/server_module"
+#   server_name = "prometheus"
+#   script_path = "install_docker_prometheus.sh"
+#   web_port    = 9090
+#   is_file_copied = true
+#   file_name = "prometheus.yml"
+# }
